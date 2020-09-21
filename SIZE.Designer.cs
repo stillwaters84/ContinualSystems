d996f1c5fb993bedd9369ca0dd3c6c20123ea367
unknown <egartsev9@gmail.com>
2020-09-21 10:24:51 +0300

    First lab

---
+++ SIZE.Designer.cs
@@ -0,0 +1,408 @@
+﻿namespace GL
+{
+    partial class SIZE
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.NumOfNodes = new System.Windows.Forms.TextBox();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.Resistors = new System.Windows.Forms.TextBox();
+            this.Capasitors = new System.Windows.Forms.TextBox();
+            this.Inductions = new System.Windows.Forms.TextBox();
+            this.ITUN = new System.Windows.Forms.TextBox();
+            this.INUN = new System.Windows.Forms.TextBox();
+            this.ITUT = new System.Windows.Forms.TextBox();
+            this.INUT = new System.Windows.Forms.TextBox();
+            this.BPTransistors = new System.Windows.Forms.TextBox();
+            this.YPTransistors = new System.Windows.Forms.TextBox();
+            this.OA = new System.Windows.Forms.TextBox();
+            this.Transformers = new System.Windows.Forms.TextBox();
+            this.IOA = new System.Windows.Forms.TextBox();
+            this.ITransformers = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // NumOfNodes
+            // 
+            this.NumOfNodes.Location = new System.Drawing.Point(3, 3);
+            this.NumOfNodes.Name = "NumOfNodes";
+            this.NumOfNodes.Size = new System.Drawing.Size(73, 20);
+            this.NumOfNodes.TabIndex = 0;
+            this.NumOfNodes.Text = "0";
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 4;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 37.38739F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 62.61261F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 91F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 127F));
+            this.tableLayoutPanel1.Controls.Add(this.NumOfNodes, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.Resistors, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.Inductions, 0, 3);
+            this.tableLayoutPanel1.Controls.Add(this.ITUN, 0, 4);
+            this.tableLayoutPanel1.Controls.Add(this.INUN, 0, 5);
+            this.tableLayoutPanel1.Controls.Add(this.ITUT, 0, 6);
+            this.tableLayoutPanel1.Controls.Add(this.INUT, 0, 7);
+            this.tableLayoutPanel1.Controls.Add(this.BPTransistors, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.YPTransistors, 2, 1);
+            this.tableLayoutPanel1.Controls.Add(this.OA, 2, 2);
+            this.tableLayoutPanel1.Controls.Add(this.Transformers, 2, 3);
+            this.tableLayoutPanel1.Controls.Add(this.IOA, 2, 4);
+            this.tableLayoutPanel1.Controls.Add(this.ITransformers, 2, 5);
+            this.tableLayoutPanel1.Controls.Add(this.label1, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.label2, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.label3, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.label4, 1, 3);
+            this.tableLayoutPanel1.Controls.Add(this.label5, 1, 4);
+            this.tableLayoutPanel1.Controls.Add(this.label6, 1, 5);
+            this.tableLayoutPanel1.Controls.Add(this.label7, 1, 6);
+            this.tableLayoutPanel1.Controls.Add(this.label8, 1, 7);
+            this.tableLayoutPanel1.Controls.Add(this.label9, 3, 0);
+            this.tableLayoutPanel1.Controls.Add(this.label10, 3, 1);
+            this.tableLayoutPanel1.Controls.Add(this.label11, 3, 2);
+            this.tableLayoutPanel1.Controls.Add(this.label12, 3, 3);
+            this.tableLayoutPanel1.Controls.Add(this.label13, 3, 4);
+            this.tableLayoutPanel1.Controls.Add(this.label14, 3, 5);
+            this.tableLayoutPanel1.Controls.Add(this.button1, 2, 7);
+            this.tableLayoutPanel1.Controls.Add(this.Capasitors, 0, 2);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(12, 12);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 8;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 28F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 26F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 27F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 28F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 28F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 124F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(430, 313);
+            this.tableLayoutPanel1.TabIndex = 1;
+            // 
+            // Resistors
+            // 
+            this.Resistors.Location = new System.Drawing.Point(3, 29);
+            this.Resistors.Name = "Resistors";
+            this.Resistors.Size = new System.Drawing.Size(73, 20);
+            this.Resistors.TabIndex = 1;
+            this.Resistors.Text = "0";
+            // 
+            // Capasitors
+            // 
+            this.Capasitors.Location = new System.Drawing.Point(3, 55);
+            this.Capasitors.Name = "Capasitors";
+            this.Capasitors.Size = new System.Drawing.Size(73, 20);
+            this.Capasitors.TabIndex = 2;
+            this.Capasitors.Text = "0";
+            // 
+            // Inductions
+            // 
+            this.Inductions.Location = new System.Drawing.Point(3, 83);
+            this.Inductions.Name = "Inductions";
+            this.Inductions.Size = new System.Drawing.Size(73, 20);
+            this.Inductions.TabIndex = 3;
+            this.Inductions.Text = "0";
+            // 
+            // ITUN
+            // 
+            this.ITUN.Location = new System.Drawing.Point(3, 109);
+            this.ITUN.Name = "ITUN";
+            this.ITUN.Size = new System.Drawing.Size(73, 20);
+            this.ITUN.TabIndex = 4;
+            this.ITUN.Text = "0";
+            // 
+            // INUN
+            // 
+            this.INUN.Location = new System.Drawing.Point(3, 136);
+            this.INUN.Name = "INUN";
+            this.INUN.Size = new System.Drawing.Size(73, 20);
+            this.INUN.TabIndex = 5;
+            this.INUN.Text = "0";
+            // 
+            // ITUT
+            // 
+            this.ITUT.Location = new System.Drawing.Point(3, 164);
+            this.ITUT.Name = "ITUT";
+            this.ITUT.Size = new System.Drawing.Size(73, 20);
+            this.ITUT.TabIndex = 6;
+            this.ITUT.Text = "0";
+            // 
+            // INUT
+            // 
+            this.INUT.Location = new System.Drawing.Point(3, 192);
+            this.INUT.Name = "INUT";
+            this.INUT.Size = new System.Drawing.Size(73, 20);
+            this.INUT.TabIndex = 7;
+            this.INUT.Text = "0";
+            // 
+            // BPTransistors
+            // 
+            this.BPTransistors.Location = new System.Drawing.Point(214, 3);
+            this.BPTransistors.Name = "BPTransistors";
+            this.BPTransistors.Size = new System.Drawing.Size(75, 20);
+            this.BPTransistors.TabIndex = 8;
+            this.BPTransistors.Text = "0";
+            // 
+            // YPTransistors
+            // 
+            this.YPTransistors.Location = new System.Drawing.Point(214, 29);
+            this.YPTransistors.Name = "YPTransistors";
+            this.YPTransistors.Size = new System.Drawing.Size(75, 20);
+            this.YPTransistors.TabIndex = 9;
+            this.YPTransistors.Text = "0";
+            // 
+            // OA
+            // 
+            this.OA.Location = new System.Drawing.Point(214, 55);
+            this.OA.Name = "OA";
+            this.OA.Size = new System.Drawing.Size(75, 20);
+            this.OA.TabIndex = 10;
+            this.OA.Text = "0";
+            // 
+            // Transformers
+            // 
+            this.Transformers.Location = new System.Drawing.Point(214, 83);
+            this.Transformers.Name = "Transformers";
+            this.Transformers.Size = new System.Drawing.Size(75, 20);
+            this.Transformers.TabIndex = 11;
+            this.Transformers.Text = "0";
+            // 
+            // IOA
+            // 
+            this.IOA.Location = new System.Drawing.Point(214, 109);
+            this.IOA.Name = "IOA";
+            this.IOA.Size = new System.Drawing.Size(75, 20);
+            this.IOA.TabIndex = 12;
+            this.IOA.Text = "0";
+            // 
+            // ITransformers
+            // 
+            this.ITransformers.Location = new System.Drawing.Point(214, 136);
+            this.ITransformers.Name = "ITransformers";
+            this.ITransformers.Size = new System.Drawing.Size(75, 20);
+            this.ITransformers.TabIndex = 13;
+            this.ITransformers.Text = "0";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(82, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 13);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "Число узлов";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(82, 26);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(63, 13);
+            this.label2.TabIndex = 15;
+            this.label2.Text = "Резисторы";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(82, 52);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(81, 13);
+            this.label3.TabIndex = 16;
+            this.label3.Text = "Конденсаторы";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(82, 80);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(84, 13);
+            this.label4.TabIndex = 17;
+            this.label4.Text = "Индуктивности";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(82, 106);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(38, 13);
+            this.label5.TabIndex = 18;
+            this.label5.Text = "ИТУН";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(82, 133);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(39, 13);
+            this.label6.TabIndex = 19;
+            this.label6.Text = "ИНУН";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(82, 161);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(37, 13);
+            this.label7.TabIndex = 20;
+            this.label7.Text = "ИТУТ";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(82, 189);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(38, 13);
+            this.label8.TabIndex = 21;
+            this.label8.Text = "ИНУТ";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(305, 0);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(94, 13);
+            this.label9.TabIndex = 22;
+            this.label9.Text = "Б/п транзисторы";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(305, 26);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(95, 13);
+            this.label10.TabIndex = 23;
+            this.label10.Text = "У/п транзисторы";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(305, 52);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(91, 13);
+            this.label11.TabIndex = 24;
+            this.label11.Text = "Опер. усилители";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(305, 80);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(97, 13);
+            this.label12.TabIndex = 25;
+            this.label12.Text = "Трансформаторы";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(305, 106);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(109, 13);
+            this.label13.TabIndex = 26;
+            this.label13.Text = "Ид. опер. усилители";
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Location = new System.Drawing.Point(305, 133);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(115, 13);
+            this.label14.TabIndex = 27;
+            this.label14.Text = "Ид. трансформаторы";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(214, 192);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 28;
+            this.button1.Text = "Ok";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // SIZE
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(454, 329);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "SIZE";
+            this.Text = "Размерность схемы";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox NumOfNodes;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TextBox Resistors;
+        private System.Windows.Forms.TextBox Capasitors;
+        private System.Windows.Forms.TextBox Inductions;
+        private System.Windows.Forms.TextBox ITUN;
+        private System.Windows.Forms.TextBox INUN;
+        private System.Windows.Forms.TextBox ITUT;
+        private System.Windows.Forms.TextBox INUT;
+        private System.Windows.Forms.TextBox BPTransistors;
+        private System.Windows.Forms.TextBox YPTransistors;
+        private System.Windows.Forms.TextBox OA;
+        private System.Windows.Forms.TextBox Transformers;
+        private System.Windows.Forms.TextBox IOA;
+        private System.Windows.Forms.TextBox ITransformers;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.Button button1;
+    }
+}
