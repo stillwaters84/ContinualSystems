5e0e55ace6e04a8b87269d1387d76d80e6c1a728
unknown <egartsev9@gmail.com>
2020-09-25 15:42:02 +0300

    Лабораторная 3

--- GV.cs
+++ GV.cs
@@ -9,7 +9,7 @@ namespace GL
     class GV
     {
         public static int M = 100, MR = 50, MF = 20;
-        public static int nv, nr, nc, nl, itun, itut, inun, inut, bpt, ypt, oa, tr, ioa, itr;
+        public static int nv, nr, nc, nl, itun, itut, inun, inut, bpt, ypt, oa, tr, ioa, itr, k;
         public static int[,] in_r = new int[MR + 1, 2];
         public static float[] z_r = new float[MR + 1];
         public static int[,] in_c = new int[MR + 1, 2];
