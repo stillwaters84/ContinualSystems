11584f0a0b151be0840c3bae87df0bf43f10c40a
unknown <egartsev9@gmail.com>
2020-09-25 20:54:49 +0300

    5 лабораторная без проверок

---
+++ F.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GL
+{
+    public partial class F : Form
+    {
+        int m_f = 0;
+        public F()
+        {
+            InitializeComponent();
+        }
+
+        private void IDC_F_CheckedChanged(object sender, EventArgs e)
+        {
+            IDC_F1.Focus();
+            m_f = 0;
+            IDC_T1.Text = "Значение частоты (кгц)";
+            IDC_T2.Text = "";
+            IDC_T3.Text = "";
+        }
+
+        private void IDC_DF_CheckedChanged(object sender, EventArgs e)
+        {
+            IDC_F1.Focus();
+            m_f = 1;
+            IDC_T1.Text = "Минимальная частота Fmin(кгц)";
+            IDC_T2.Text = "Максимальная частота Fmax(кгц)";
+            IDC_T3.Text = "Шаг изменения частоты dF(кгц)";
+        }
+
+        private void IDC_KF_CheckedChanged(object sender, EventArgs e)
+        {
+            IDC_F1.Focus();
+            m_f = 2;
+            IDC_T1.Text = "Минимальная частота Fmin(кгц)";
+            IDC_T2.Text = "Максимальная частота Fmax(кгц)";
+            IDC_T3.Text = "Отношение соседних частот K";
+        }
+
+        private void IDC_FOK_BUTTON_Click(object sender, EventArgs e)
+        {
+            float fmin, fmax, df, kk;
+            int kf;
+            switch (m_f)
+            {
+                case 0:
+                    GV.f[1] = float.Parse(IDC_F1.Text);
+                    GV.nf = 1;
+                    break;
+                case 1:
+                    fmin = GV.f[1] = float.Parse(IDC_F1.Text);
+                    fmax = float.Parse(IDC_F2.Text);
+                    df = float.Parse(IDC_F3.Text);
+                    kf = 1;
+                    while (GV.f[kf] < fmax)
+                    {
+                        GV.f[kf + 1] = GV.f[kf] + df;
+                        kf = kf + 1;
+                    }
+                    GV.nf = kf;
+                    break;
+                case 2:
+                    GV.f[1] = float.Parse(IDC_F1.Text);
+                    fmax = float.Parse(IDC_F2.Text);
+                    kk = float.Parse(IDC_F3.Text);
+                    kf = 1;
+                    while (GV.f[kf] < fmax)
+                    {
+                        GV.f[kf + 1] = kk * GV.f[kf];
+                        kf = kf + 1;
+                    }
+                    GV.nf = kf;
+                    break;
+            }
+            this.Close();
+        }
+    }
+}
---
+++ IO.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GL
+{
+    public partial class IO : Form
+    {
+        public IO()
+        {
+            InitializeComponent();
+        }
+
+        private void IDC_IOOK_BUTTON_Click(object sender, EventArgs e)
+        {
+            GV.lp = Int32.Parse(m_lp.Text);
+            GV.lm = Int32.Parse(m_lm.Text);
+            GV.kp = Int32.Parse(m_kp.Text);
+            GV.km = Int32.Parse(m_km.Text);
+            this.Close();
+        }
+    }
+}
